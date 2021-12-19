bb693e05195a77c6d7725c7e92d8060d113f6d0a
Lakshmidharan <lakshmidharan2401@gmail.com>
2021-12-19 08:08:35 -0800

    commit1

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 import Card from './State';
+import { useState } from 'react';
 
 function App() {
   return (
     <div className="App">
-      <Card></Card>
+      <div class="container">
+        <div class="row">
+          <Card></Card>
+        </div>
+      </div>
     </div>
   );
 }
-
 export default App;
--- src/State.js
+++ src/State.js
@@ -1,5 +1,58 @@
+import { useState } from 'react';
 export default function Card(){
-    return<div>
-        <h1>Hello world</h1>
+    const [value,setValue]=useState([]);
+    let add= () =>{setValue([...value,`${value.length}`])}
+    return(<div> 
+        <ul>
+        {
+          value.map((item) => <li>{item}</li>)
+        }
+      </ul>
+     
+        <div>
+
+            
+        <div class="col-md-12">
+            <div class="card card-white">
+                <div class="card-body">
+                    <form action="javascript:void(0);">
+                        <input type="text" class="form-control add-task" placeholder="New Task..."/>
+                    </form>
+                    <ul class="nav nav-pills todo-nav">
+                        <li role="presentation" class="nav-item all-task active"><a href="#" class="nav-link">All</a></li>
+                        <li role="presentation" class="nav-item active-task"><a href="#" class="nav-link">Active</a></li>
+                        <li role="presentation" class="nav-item completed-task"><a href="#" class="nav-link">Completed</a></li>
+                    </ul>
+                    <div class="todo-list">
+                        <div class="todo-item">
+                            <div class="checker"><span class=""><input type="checkbox"/></span></div>
+                            <span>Create theme</span>
+                            <a href="javascript:void(0);" class="float-right remove-todo-item"><i class="icon-close"></i></a>
+                        </div>
+                        <div class="todo-item">
+                            <div class="checker"><span class=""><input type="checkbox"/></span></div>
+                            <span>Work on wordpress</span>
+                            <a href="javascript:void(0);" class="float-right remove-todo-item"><i class="icon-close"></i></a>
+                        </div>
+                        
+                        <div class="todo-item">
+                            <div class="checker"><span class=""><input type="checkbox"/></span></div>
+                            <span>Organize office main department</span>
+                            <a href="javascript:void(0);" class="float-right remove-todo-item"><i class="icon-close"></i></a>
+                        </div>
+                        <div class="todo-item">
+                            <div class="checker"><span><input type="checkbox"/></span></div>
+                            <span>Error solve in HTML template</span>
+                            <a href="javascript:void(0);" class="float-right remove-todo-item"><i class="icon-close"></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+            
+            <button onClick={add}>Add</button>
+            
+      </div>
     </div>
-}
+    )
+    }
